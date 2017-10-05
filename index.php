c85ee145283ad2e746dc22597fa15329efc2da7a
gdm-1718-liamdeke <liamdeke@student.arteveldehs.be>
2017-10-05 10:35:00 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+    $name = 'Liam De Kegel';
+
+    $string1 = 'Hello';
+    $string2 = 'World';
+
+    $x = 7;
+    $y = 10;
+
+    $watIs ='stirng';
+
+    $age = 21;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div class= "Wiskunde">
+        <?= $y ." + ". $x ." = ". ($x + $y) ?> </br>
+        <?= $y ." - ". $x ." = ". ($y - $x) ?> </br>
+        <?= $y ." * ". $x ." = ". ($x * $y) ?> </br>
+        <?= $y ." / ". $x ." = ". ($y / $x) ?> </br>
+        <?= $y ." % ". $x ." = ". ($y % $x) ?>
+    </div>
+</body>
+</html>
